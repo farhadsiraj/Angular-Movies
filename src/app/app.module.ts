c38db4d711636224aa6fa1caa336f470aa8fc537
Farhad Siraj <farhadsiraj@gmail.com>
2022-09-08 16:29:16 -0400

    Added animations to the slider component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SliderComponent } from './components/slider/slider.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MoviesComponent, SliderComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MoviesComponent, SliderComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
