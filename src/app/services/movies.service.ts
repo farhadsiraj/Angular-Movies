202ae39fb8e2a3d2b6dd90c989c04ba1b11389cc
Farhad Siraj <farhadsiraj@gmail.com>
2022-09-12 19:44:19 -0400

    Changed shape of observable type with rxjs library

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MovieData } from '../models/movie';
+import { switchMap, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +11,15 @@ export class MoviesService {
 
   constructor(private http: HttpClient) {}
 
+  /* .pipe() and of() is part of the rxjs library
+      .pipe() lets you change the shape of an Observable
+      of() lets you change something into an Observable
+  */
   getMovies(type: string = 'upcoming') {
-    return this.http.get<MovieData>(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`);
+    return this.http.get<MovieData>(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`).pipe(
+      switchMap((res) => {
+        return of(res.results);
+      })
+    );
   }
 }
