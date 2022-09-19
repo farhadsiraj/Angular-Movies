af7378de9fd750742a16c745151da0d754701d4c
Farhad Siraj <farhadsiraj@gmail.com>
2022-09-19 15:10:28 -0400

    Created movie review models

--- src/app/models/movie.ts
+++ src/app/models/movie.ts
@@ -56,6 +56,23 @@ export interface MovieCredits {
   }[];
 }
 
+export interface MovieReviewData {
+  id: number;
+  results: MovieReviews[];
+  page: number;
+  total_results: number;
+  total_pages: number;
+}
+
+export interface MovieReviews {
+  author: string;
+  author_details: object;
+  content: string;
+  created_at: string;
+  id: string;
+  updated_at: string;
+  url: string;
+}
 // export const mapMovieToItem = (movie: Movie): Item => {
 //   return {
 //     id: movie.id,
