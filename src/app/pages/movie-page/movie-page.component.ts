fe7ffdc297ff40214a0238c8e3da4996ae136726
Farhad Siraj <farhadsiraj@gmail.com>
2022-09-19 13:58:42 -0400

    Retreived movieVideoData from api

--- src/app/pages/movie-page/movie-page.component.ts
+++ src/app/pages/movie-page/movie-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Movie } from 'src/app/models/movie';
+import { Movie, MovieVideo } from 'src/app/models/movie';
 import { MoviesService } from 'src/app/services/movies.service';
 import { IMAGE_SIZES } from '../../constants/image-sizes';
 import ISO6391 from 'iso-639-1';
@@ -12,6 +12,7 @@ import ISO6391 from 'iso-639-1';
 })
 export class MoviePageComponent implements OnInit {
   movie: Movie | null = null;
+  movieVideos: MovieVideo[] = [];
   imageSizes = IMAGE_SIZES;
 
   constructor(private route: ActivatedRoute, private moviesService: MoviesService) {}
@@ -19,6 +20,7 @@ export class MoviePageComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe(({ id }) => {
       this.getMovie(id);
+      this.getMovieVideos(id);
     });
   }
 
@@ -26,6 +28,12 @@ export class MoviePageComponent implements OnInit {
     this.moviesService.getMovieDetails(id).subscribe((movieData) => {
       this.movie = movieData;
       this.movie.original_language = ISO6391.getName(this.movie.original_language);
+    });
+  }
+
+  getMovieVideos(id: string) {
+    this.moviesService.getMovieVideos(id).subscribe((movieVideoData) => {
+      this.movieVideos = movieVideoData;
     });
   }
 }
