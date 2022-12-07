965a734ef6e5f9549607af3f698396cce9702a2c
Farhad Siraj <farhadsiraj@gmail.com>
2022-12-07 15:33:26 -0500

    Created tvshow component

---
+++ src/app/pages/tvshow/tvshow.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Item } from 'src/app/components/item/item';
+import { mapTvShowToItem, TvShow, TvShowCredits, TvShowImages, TvShowVideo } from 'src/app/models/show';
+import { TvShowsService } from 'src/app/services/tvshows.service';
+import { IMAGE_SIZES } from '../../constants/image-sizes';
+
+@Component({
+  selector: 'app-tvShow',
+  templateUrl: './tvShow.component.html',
+  styleUrls: ['./tvShow.component.scss']
+})
+export class TvShowComponent implements OnInit, OnDestroy {
+  tvShow: TvShow | null = null;
+  tvShowBanner: Item | null = null;
+  tvShowVideos: TvShowVideo[] = [];
+  tvShowImages: TvShowImages | null = null;
+  tvShowCredits: TvShowCredits | null = null;
+  imagesSizes = IMAGE_SIZES;
+
+  constructor(private route: ActivatedRoute, private tvShowsService: TvShowsService) {}
+
+  ngOnInit(): void {
+    this.route.params.pipe().subscribe(({ id }) => {
+      this.getTvShow(id);
+      this.getTvShowVideos(id);
+      this.getTvShowImages(id);
+      this.getTvShowCredits(id);
+    });
+  }
+
+  ngOnDestroy() {
+    console.log('component destroyed');
+  }
+
+  getTvShow(id: string) {
+    this.tvShowsService.getTvShow(id).subscribe((tvShowData) => {
+      this.tvShowBanner = mapTvShowToItem(tvShowData);
+      this.tvShow = tvShowData;
+    });
+  }
+
+  getTvShowVideos(id: string) {
+    this.tvShowsService.getTvShowVideos(id).subscribe((tvShowVideosData) => {
+      this.tvShowVideos = tvShowVideosData;
+    });
+  }
+
+  getTvShowImages(id: string) {
+    this.tvShowsService.getTvShowImages(id).subscribe((tvShowImagesData) => {
+      this.tvShowImages = tvShowImagesData;
+    });
+  }
+
+  getTvShowCredits(id: string) {
+    this.tvShowsService.getTvShowCredits(id).subscribe((tvShowCreditsData) => {
+      this.tvShowCredits = tvShowCreditsData;
+    });
+  }
+}
